6d9a3bae4ce16f1d5921a5dfbce1551110a1cf4e
mawegrzyn-ux <ma.wegrzyn@gmail.com>
2025-08-17 19:56:34 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+// ⬇️ Import the routing wrapper instead of App directly
+import Main from "./Main.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
