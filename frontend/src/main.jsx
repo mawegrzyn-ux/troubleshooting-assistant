2f179378bfb6e9ec65bb46a0678f8a5ae8838c1e
mawegrzyn-ux <ma.wegrzyn@gmail.com>
2025-08-17 20:04:16 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
-import Admin from './Admin.jsx';
+import AdminPanel from './AdminPanel.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
